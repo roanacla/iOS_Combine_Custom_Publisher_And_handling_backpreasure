f200da9bd959288e6f7536c7b8831fbd571db0f0
roanacla <roanacla@me.com>
2020-07-07 16:29:54 -0700

    unwrap operator

--- Starter.playground/Pages/Unwrap operator.xcplaygroundpage/Contents.swift
+++ Starter.playground/Pages/Unwrap operator.xcplaygroundpage/Contents.swift
@@ -1,34 +1,23 @@
 import Combine
 
-<#Add your code here#>
+extension Publisher {
+  // 1 The T is to specify Generics. CompactMap<Upstream, Output> is equivalent now to CompactMap<Self, T> plus the conditional Output == Optional<T>
+  func unwrap<T>() -> Publishers.CompactMap<Self, T> where Output == Optional<T> {
+    // 2
+    compactMap { $0 }
+  }
+}
 
-//: [Next](@next)
-/*:
- Copyright (c) 2019 Razeware LLC
+let values: [Int?] = [1, 2, nil, 3, nil, 4]
 
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
+values.publisher
+  .unwrap()
+  .sink {
+    print("Received value: \($0)")
+  }
 
- The above copyright notice and this permission notice shall be included in
- all copies or substantial portions of the Software.
-
- Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
- distribute, sublicense, create a derivative work, and/or sell copies of the
- Software in any work that is designed, intended, or marketed for pedagogical or
- instructional purposes related to programming, coding, application development,
- or information technology.  Permission for such use, copying, modification,
- merger, publication, distribution, sublicensing, creation of derivative works,
- or sale is expressly withheld.
-
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- THE SOFTWARE.
- */
+//########### OUTPUT #############
+//Received value: 1
+//Received value: 2
+//Received value: 3
+//Received value: 4
